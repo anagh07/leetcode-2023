1ed8336d4169b2b24ea26bcc42892dbc8a4a4ffb
anagh <anagh07@gmail.com>
2024-05-05 11:12:10 -0700

    ValidAnagram optimize solution

--- src/ArraysAndHashing/ValidAnagram.java
+++ src/ArraysAndHashing/ValidAnagram.java
@@ -9,9 +9,9 @@ public class ValidAnagram {
     public static void main(String[] args) {
         // Input: s = "anagram", t = "nagaram"
         // s = "rat", t = "car"
-        String s = "ccac";
-        String t = "aacc";
-        System.out.println(isAlsoAnagram(s, t));
+        String s = "anagram";
+        String t = "nagaram";
+        System.out.println(isEfficientAnagram(s, t));
     }
 
     public static boolean isAnagram(String s, String t) {
@@ -29,6 +29,30 @@ public class ValidAnagram {
         return true;
     }
 
+    public static boolean isEfficientAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+
+            map.computeIfPresent(sChar, (key, val) -> val += 1);
+            map.computeIfAbsent(sChar, (key) -> 1);
+            map.computeIfPresent(tChar, (key, val) -> val -= 1);
+            map.computeIfAbsent(tChar, (key) -> -1);
+        }
+
+        for (Character letter : map.keySet()) {
+            if (map.get(letter) != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static boolean isAlsoAnagram(String s, String t) {
         if (s.length() != t.length()) {
             return false;
@@ -37,14 +61,14 @@ public class ValidAnagram {
         HashMap<Character, Integer> wordT = new HashMap<Character, Integer>();
         for (Character letter : s.toCharArray()) {
             if (wordS.containsKey(letter)) {
-                wordS.put(letter, wordS.get(letter)+1);
+                wordS.put(letter, wordS.get(letter) + 1);
             } else {
                 wordS.put(letter, 1);
             }
         }
         for (Character letter : t.toCharArray()) {
             if (wordT.containsKey(letter)) {
-                wordT.put(letter, wordT.get(letter)+1);
+                wordT.put(letter, wordT.get(letter) + 1);
             } else {
                 wordT.put(letter, 1);
             }
