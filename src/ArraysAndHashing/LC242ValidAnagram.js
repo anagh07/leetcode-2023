b5f07579463a20075424245aa5e61b82fac7a95b
Anagh Mehran <anagh07@gmail.com>
2024-11-12 22:40:33 -0800

    LC242 - Valid Anagram JS optimized solution

--- src/ArraysAndHashing/LC242ValidAnagram.js
+++ src/ArraysAndHashing/LC242ValidAnagram.js
@@ -23,5 +23,38 @@ var isAnagram = function (s, t) {
     return true;
 };
 
+/**
+ * 
+ * @param {string} s 
+ * @param {string} t 
+ * @returns {boolean}
+ */
+let isAnagramOptimized = (s, t) => {
+    let count = Array(26).fill(0)
+
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        // console.log(count[s.charCodeAt(i) - 'a'.charCodeAt(0)]);
+        count[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+        count[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
+    }
+
+    for (let num of count) {
+        if (num !== 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// Using basic solution
 console.log(isAnagram('anagram', 'nagaram'));
 console.log(isAnagram('rat', 'car'));
+
+// Using optimized solution
+console.log(isAnagramOptimized('anagram', 'nagaram'));
+console.log(isAnagramOptimized('rat', 'car'));
